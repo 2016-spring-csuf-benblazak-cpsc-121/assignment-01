20d390555fd1b0153c3bc55f962edfe91c087044
zheanguizar99 <zheanguizar99@yahoo.com>
2016-02-05 13:43:38 -0800

    finished assignment

--- solution--zheanguizar99/main.cpp
+++ solution--zheanguizar99/main.cpp
@@ -1,4 +1,45 @@
 #include <iostream>
 #include <string>
+ 
+using std::cout;
+using std::cin;
+using std::char;
+using std::string;
 
-int main()
+string convert(int num);
+
+int main(){
+  
+  int num;
+  char choice;
+  
+  do{
+    cout << "Please enter an integer. ";
+    cin >> num;
+    cout << convert(num) << endl;
+    
+    cout << "Would you like to enter another integer? (Y/N)";
+    cin >> choice;
+    cout << endl;
+  } while(choice == 'Y' || choice == 'y')
+  
+  return 0;
+  
+}
+
+string convert(int num){
+  
+  const string numeral [] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "VI", "I"};
+  const int value [] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+  
+  string ret = "";
+  
+  for(int i = 0; i < 13; i++){
+    for(int j = 0; j < num/values[i]; j++)
+      ret += numeral[i];
+      num %= value[i];
+  }
+  
+  return ret;
+  
+} 
