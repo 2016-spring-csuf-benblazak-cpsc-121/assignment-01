f9f6fc285fc649ba6851d0318165e80ea9e4e5d6
vuchampion <vuchampion@csu.fullerton.edu>
2016-02-04 17:07:56 -0800

    Create main.cpp

---
+++ solution--darren.vu./main.cpp
@@ -0,0 +1,6 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Ben Blazak <bblazak@fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+Worked with Steven Kha
