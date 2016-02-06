a6d0a01c57616f65297c7db73f631e1e17dc998b
HaganeKent <kentat73@gmail.com>
2016-02-06 11:04:28 -0800

    Update main.cpp

--- Solution--KenTat/main.cpp
+++ Solution--KenTat/main.cpp
@@ -1,1 +1,20 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Ken Tat <ktat73@csu.fullerton.edu>, CPSC 121 Class
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
 
+/**
+* A program that converts a user inputted number into roman numerals.
+*/
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+  int number;
+  char roman;
+}
