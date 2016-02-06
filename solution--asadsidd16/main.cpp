d1e9b943b828e1ddf1c10cd16f53b1ab342bde86
asadsidd16 <asadsidd16@csu.fullerton.edu>
2016-02-06 15:33:58 -0800

    Update main.cpp

--- solution--asadsidd16/main.cpp
+++ solution--asadsidd16/main.cpp
@@ -6,3 +6,20 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+
+int main(){
+  int num;
+  char yes = 'Y';
+  int numerals =[5000];
+  
+    while(i >=5000){
+    if (i >= 1000)
+    cout << "M";
+    else if ()
+    
+  }
+  cout << "Please enter number: ";
+  cin >> num;
+  
+  return 0;
+}
