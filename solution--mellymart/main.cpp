130820531a0fb90a2b8fc3839a3a0171b3651593
mellymart <martinez-melissa1@csu.fullerton.edu>
2016-02-06 22:57:02 -0800

    Assignment 01

---
+++ solution--mellymart/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using std :: cout;
+using std :: cin;
+using std :: endl;
+using std :: string;
+int main()
+string convert(int num);
+{
+	while (answer=='y'||answer== 'Y')
+	{
+		int num;
+		char answer;
+		cout << "enter an integer";
+		cin >> num;
+		cout << convert(num) << endl;
+		cout << " would you like to convert another?";
+		cin >> answer;
+
+		if (answer == 'n' || answer == 'N;')
+			break;
+	}
+	string convert(int num)
+		const numerals[] = {
+		"M","CM","D","CD","C","XE","L","XL","X","IX","V","IV","I",
+	};
+	const int values[] = {
+		1000,900,500,400,100,90,50,40,10,9,5,4,1,
+	};
+	
+	
+	cin.get();
+	return 0;
+}
