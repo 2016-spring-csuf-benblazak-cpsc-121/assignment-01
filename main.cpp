dd6fc3bfadaa67ab0f9979e23db1421660a0e16c
phuynh86 <phuynh86@csu.fullerton.edu>
2016-02-06 21:27:21 -0800

    finished assignment-01

---
+++ main.cpp
@@ -0,0 +1,62 @@
+/* ----------------------------------------------------------------------------
+ * Copyright &copy; 2016 Patrick Huynh <phuynh86@csu.fullerton.edu>
+ * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+ * ------------------------------------------------------------------------- */
+
+/**
+ * A short interactive program that converts Arabic integers to Roman Numerals.
+ */
+ 
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+string convert(int num);
+int main(){
+    char response;
+
+    cout << "Enter a number";
+    cin >> num;
+    cout << convert(num) << endl;
+    cout << "Continue? (Y/N)";
+    cin >> response;
+    cout << "\n";
+    if (response == 'N' || response == 'n'){
+      break;
+    else{
+        int num == j;
+        string return = "";
+        if (j<=0 || j>=4000)
+              return "error";
+        else{
+            for(int j=1000; j<=1000, j-=0;)
+            std::cout << "M" << endl;
+            for(int j=900; j<=900, j-=0;)
+            std::cout << "CM" << endl;
+            for(int j=500; j<=500, j-=0;)
+            std::cout << "D" << endl;
+            for(int j=400; j<=400, j-=0;)
+            std::cout << "CD" << endl;
+            for(int j=100; j<=100, j-=0;)
+            std::cout << "C" << endl;
+            for(int j=90; j<=90, j-=0;)
+            std::cout << "XC" << endl;
+            for(int j=50; j<=50, j-=0;)
+            std::cout << "L" << endl;
+            for(int j=40; j<=40, j-=0;)
+            std::cout << "XL" << endl;
+            for(int j=10; j<=10, j-=0;)
+            std::cout << "X" << endl;
+            for(int j=9; j<=9, j-=0;)
+            std::cout << "IX" << endl;
+            for(int j=5; j<=5, j-=0;)
+            std::cout << "V" << endl;
+            for(int j=4; j<=4, j-=0;)
+            std::cout << "IV" << endl;
+            for(int j=1; j<=1, j-=0;)
+            std::cout << "I" << endl;
+        
+        return 0;
+        }
+    }
