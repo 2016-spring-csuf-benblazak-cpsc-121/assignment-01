643ffd6d6a249cf4ad3425e59d873fe839e766cc
HaganeKent <kentat73@gmail.com>
2016-02-06 17:56:22 -0800

    Update main.cpp

--- Solution--KenTat/main.cpp
+++ Solution--KenTat/main.cpp
@@ -15,6 +15,44 @@ using std::string;
 
 int main()
 {
+
   int number;
   char roman;
+  char repeat;
+  cout << "Number to Roman Numeral Conversion" << endl;
+  while(true)
+  {
+  cout << "Please enter a number for conversion: ";
+  cin >> number;
+  cout << endl;
+  cout << "Converted number: " << convert(number) << endl;
+  cout << "Do you want to convert another number? Answer Y or N." << endl;
+  cin >> repeat;
+  
+  for (int i = 0, i < 13, i++)
+  {
+    while (number >= value[i])
+    {
+      ret += romannumeral[i]
+      roman = romannumeral[i]
+    }
+  }
+  string convert(int number)
+  {
+    const string romannumeral [] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "VI", "I"};
+    const int value [] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    string ret = "";
+    
+  }
+  
+  if (number > 3999 || number < 1)
+  return "Invalid number for conversion, between 1 - 3999" << endl;
+  
+  if (repeat == 'n' || repeat == 'N')
+  break;
+  else if (repeat == 'y' || repeat == 'Y')
+  return 0;
+  }
+  return 0;
 }
+
