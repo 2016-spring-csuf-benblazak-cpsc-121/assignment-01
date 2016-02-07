1888abdf697c013465681567ab34d11f24a81d24
jacquelinea623 <andrade633@csu.fullerton.edu>
2016-02-06 23:00:10 -0800

    finished assignment

---
+++ solution--jacquelinea623/main.cpp
@@ -0,0 +1,20 @@
+int main() 
+{
+while ('y' || 'Y')
+
+
+cout<< "Please enter an integer:";
+cin>> num
+convert (num);
+cout <<"romannumerals" <<end1;
+cout<< "would you like to convert (y/n)?";
+cin>> answer;
+cin>>I; 
+cout<<'1';
+cin>>II; 
+cout<<'2'
+
+}
+systen("pause");
+return 0;
+
